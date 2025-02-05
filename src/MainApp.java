69b3ee1df1dae1b185fca2f6af7e8f03780d26da
Weslley Ribeiro <weslleysilvaribeiro35@gmail.com>
2025-02-05 18:08:12 -0300

    feat: entrada de dados, saida de dados e processamento de dados

--- src/MainApp.java
+++
@@ -1,6 +0,0 @@
-public class MainApp {
-    public static void main(String[] args) {
-        short x = 1;
-        System.out.println(x);
-    }
-}
---
+++ src/operacoesDeSaida.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+
+public class operacoesDeES {
+    public static void main(String[] args) {
+
+        // Operacoes de Saida
+        System.out.println("Printa pulando a proxima linha");
+        System.out.print("Apenas printa");
+        double x = 3.12414;
+        System.out.printf("%nFormata: %.2f\n", x);
+        Locale.setDefault(Locale.US);
+        System.out.printf("%nFormata: %.4f\n", x);
+
+        // CONCATENACOES DE STRINGS
+        System.out.println("Resultado " + x + " em metros");
+        System.out.printf("Resultado %.2f em metros\n", x);
+
+        String nome = "Maria";
+        int idade = 31;
+        double saldo = 4000.124;
+
+        System.out.printf("%s tem %d anos e possui %.2f de saldo na conta", nome,idade, saldo);
+    }
+}
